dc882b63b191ba627537b37c84a4dc16a8a19827
psyomn <lethaljellybean@gmail.com>
2013-07-18 00:12:34 -0400

    Add method checks for version_history.rb

--- spec/version_history_spec.rb
+++ spec/version_history_spec.rb
@@ -1,4 +1,34 @@
+require 'turntables/version_history'
 
 describe VersionHistory do 
 
+  context "<<AR>> pattern specification" do
+    it "should respond to class method check" do
+      VersionHistory.should respond_to(:check)
+    end
+
+    it "should respond to class method find_all" do
+      VersionHistory.should respond_to(:find_all)
+    end
+
+    it "should respond to class method find_last" do
+      VersionHistory.should respond_to(:find_last)
+    end
+
+    it "should respond to class method find" do
+      VersionHistory.should respond_to(:find)
+    end
+
+    it "should respond to class method insert" do
+      VersionHistory.should respond_to(:insert)
+    end
+
+    it "should respond to class method pull_up!" do
+      VersionHistory.should respond_to(:pull_up!)
+    end
+    
+    it "should respond to class method to_version_history" do
+      VersionHistory.should respond_to(:to_version_history)
+    end
+  end
 end
